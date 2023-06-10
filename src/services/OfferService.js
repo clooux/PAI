92afeb00bf15ebb45aa0150c31bcc95e7d55d7d2
clooux <55985737+clooux@users.noreply.github.com>
2023-06-10 23:34:59 +0200

    deleting unused functions

--- src/services/OfferService.js
+++ src/services/OfferService.js
@@ -1,18 +1,6 @@
 const daoAuction = require("../models/dao/AuctionDAO");
 const daoOffer = require("../models/dao/OfferDAO");
 
-const getAllOffers = async () => {
-  const offers = await daoOffer.findAll();
-  // Operacje przetworzenia tych danych
-  return offers;
-};
-
-const getOffersByAuctionId = async (auctionId) => {
-  const offers = await daoOffer.findAllByAuctionId(auctionId);
-
-  return offers;
-};
-
 const postOffer = async (id, offer) => {
   const auction = await daoAuction.findById(id);
   if (!auction) {
