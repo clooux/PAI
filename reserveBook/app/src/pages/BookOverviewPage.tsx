eb1b610165516a460f2bbe448cd0902db4237dff
mmysliwiec2112 <michal.mysliwiec@student.uj.edu.pl>
2023-07-11 16:04:20 +0200

    book overview page mockup

--- reserveBook/app/src/pages/BookOverviewPage.tsx
+++ reserveBook/app/src/pages/BookOverviewPage.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import img from "../files/7965b828-6745-45a6-a2ed-a9d1869d97fc.jpg"
+import '../styles/bookOverview.css'
+function BookPageItem() {
+    return <div className="card w-96 bg-base-100 shadow-xl">
+        <figure><img src={img} alt="Book"/></figure>
+        <div className="card-body">
+            <h2 className="card-title">Book</h2>
+            <p>Lorem ipsum dolor sit amet</p>
+            <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+    </div>
+}
 
 function BookOverviewPage() {
-  return <div>BooksPagOverview</div>;
+    return <div className="books-overview-wrapper">
+        <div className="grid-container">
+            <div className="grid-item">{BookPageItem()}</div>
+            <div className="grid-item">{BookPageItem()}</div>
+            <div className="grid-item">{BookPageItem()}</div>
+            <div className="grid-item">{BookPageItem()}</div>
+            <div className="grid-item">{BookPageItem()}</div>
+            <div className="grid-item">{BookPageItem()}</div>
+            <div className="grid-item">{BookPageItem()}</div>
+            <div className="grid-item">{BookPageItem()}</div>
+            <div className="grid-item">{BookPageItem()}</div>
+        </div>
+    </div>;
 }
 
 export default BookOverviewPage;
