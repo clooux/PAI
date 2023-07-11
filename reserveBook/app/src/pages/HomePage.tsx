40f6e06f98ec3083b0ae93c3953c711ef45115b4
mmysliwiec2112 <michal.mysliwiec@student.uj.edu.pl>
2023-07-11 15:45:39 +0200

    front page mockup

--- reserveBook/app/src/pages/HomePage.tsx
+++ reserveBook/app/src/pages/HomePage.tsx
@@ -1,7 +1,39 @@
 import React from "react";
-
+import img from "../files/7965b828-6745-45a6-a2ed-a9d1869d97fc.jpg"
+import '../styles/homepage.css'
 function HomePage() {
-  return <div>Home</div>;
+    return <div>
+        <div className="front-screen-title">
+            <h1>reserveBook</h1>
+            <h2>rent or buy a book!</h2>
+        </div>
+
+        <div className="carousel-wrapper">
+            <div className="carousel carousel-center rounded-box">
+                <div className="carousel-item">
+                    <img src={img} alt="Pizza"/>
+                </div>
+                <div className="carousel-item">
+                    <img src={img} alt="Pizza"/>
+                </div>
+                <div className="carousel-item">
+                    <img src={img} alt="Pizza"/>
+                </div>
+                <div className="carousel-item">
+                    <img src={img} alt="Pizza"/>
+                </div>
+                <div className="carousel-item">
+                    <img src={img} alt="Pizza"/>
+                </div>
+                <div className="carousel-item">
+                    <img src={img} alt="Pizza"/>
+                </div>
+                <div className="carousel-item">
+                    <img src={img} alt="Pizza"/>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export default HomePage;
