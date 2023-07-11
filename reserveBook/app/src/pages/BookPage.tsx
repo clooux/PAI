dbfe8f5215e090ad3790a735ffd56ce7208aee7d
mmysliwiec2112 <michal.mysliwiec@student.uj.edu.pl>
2023-07-11 17:29:15 +0200

    book page view mockup

--- reserveBook/app/src/pages/BookPage.tsx
+++ reserveBook/app/src/pages/BookPage.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import img from "../files/7965b828-6745-45a6-a2ed-a9d1869d97fc.jpg"
+import '../styles/bookPage.css'
 
 function BookPage() {
-  return (
-    <div>BookPage</div>
-  )
+    return <div className="book-page">
+        <div className="book-details">
+            <h1 className="book-title">Clean Code - A Handbook of Agile Software Craftsmanship</h1>
+            <img src={img} alt="Book Cover" className="book-cover"/>
+            <div className="book-trivia">
+                <p className="book-author">R. C. Martin</p>
+                <p className="book-desc">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum."
+                </p>
+                <button className="btn btn-warning rent-btn">Rent</button>
+            </div>
+        </div>
+
+    </div>
 }
 
 export default BookPage
+
--- reserveBook/app/src/pages/HomePage.tsx
+++ reserveBook/app/src/pages/HomePage.tsx
@@ -1,39 +1,50 @@
 import React from "react";
 import img from "../files/7965b828-6745-45a6-a2ed-a9d1869d97fc.jpg"
-import '../styles/homepage.css'
+import '../styles/homePage.css'
+
 function HomePage() {
     return <div>
-        <div className="front-screen-title">
-            <h1>reserveBook</h1>
-            <h2>rent or buy a book!</h2>
-        </div>
 
-        <div className="carousel-wrapper">
-            <div className="carousel carousel-center rounded-box">
-                <div className="carousel-item">
-                    <img src={img} alt="Book"/>
-                </div>
-                <div className="carousel-item">
-                    <img src={img} alt="Book"/>
-                </div>
-                <div className="carousel-item">
-                    <img src={img} alt="Book"/>
-                </div>
-                <div className="carousel-item">
-                    <img src={img} alt="Book"/>
-                </div>
-                <div className="carousel-item">
-                    <img src={img} alt="Book"/>
-                </div>
-                <div className="carousel-item">
-                    <img src={img} alt="Book"/>
-                </div>
-                <div className="carousel-item">
-                    <img src={img} alt="Book"/>
+        <div className="hero min-h-screen"
+             style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <div className="front-screen-title">
+                        <h1>reserveBook</h1>
+                        <h2>rent or buy a book!</h2>
+                    </div>
+                    <div className="carousel-wrapper">
+                        <div className="carousel carousel-center rounded-box">
+                            <div className="carousel-item">
+                                <img src={img} alt="Book"/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={img} alt="Book"/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={img} alt="Book"/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={img} alt="Book"/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={img} alt="Book"/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={img} alt="Book"/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={img} alt="Book"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 }
 
 export default HomePage;
