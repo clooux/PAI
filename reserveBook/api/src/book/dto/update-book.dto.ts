0ab3849ab625b6e213055b3166cc69469482b2eb
clooux <walka.adam@proton.me>
2023-07-16 15:06:19 +0200

    serve static files, changing book schema

--- reserveBook/api/src/book/dto/update-book.dto.ts
+++ reserveBook/api/src/book/dto/update-book.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create-book.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
+export class UpdateBookDto extends PartialType(CreateBookDto) {
+  @ApiProperty()
+  bookCoverId: number;
+}
